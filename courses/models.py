48abbda4fa06269e008e5ddaa23c351fdcfdda0c
JennaFi <fi@mail.com>
2025-01-17 01:15:21 +0100

    hw 30_1

--- courses/models.py
+++ courses/models.py
@@ -1,18 +1,35 @@
 from django.db import models
 
+
 class Course(models.Model):
     name = models.CharField(max_length=255, verbose_name='Course Name')
     description = models.TextField(blank=True, null=True, verbose_name='Course Description')
-    preview = models.ImageField(upload_to='courses/preview/%Y/%m/%d', blank=True, null=True, verbose_name='Course Preview')
+    preview = models.ImageField(upload_to='courses/preview/%Y/%m/%d', blank=True, null=True,
+                                verbose_name='Course Preview')
 
     class Meta:
         verbose_name = 'Course'
         verbose_name_plural = 'Courses'
+        ordering = ['name']
 
-        def __str__(self):
-            return self.name
+
+
+    def __str__(self):
+        return self.name
+
 
 class Lesson(models.Model):
     name = models.CharField(max_length=255, verbose_name='Lesson Name')
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons', verbose_name='Course')
+    description = models.TextField(blank=True, null=True, verbose_name='Lesson Description')
+    video_url = models.URLField(blank=True, null=True, verbose_name='Video URL')
+    preview = models.ImageField(upload_to='lessons/preview/%Y/%m/%d', blank=True, null=True,
+                                verbose_name='Lesson Preview')
 
+    class Meta:
+        verbose_name = 'Lesson'
+        verbose_name_plural = 'Lessons'
+        ordering = ['course']  # Order by course name by default in admin interface
+
+    def __str__(self):
+        return self.name + ' ' + self.course
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, DestroyAPIView
 
-# Create your views here.
+from courses.models import Course, Lesson
+from courses.serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class LessonCreateApiView(CreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+class LessonListAPIView(ListAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+class LessonRetrieveApiView(UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+class LessonDestroyApiView(DestroyAPIView):
+    queryset = Lesson.objects.all()
+
+class LessonUpdateApiView(UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+
