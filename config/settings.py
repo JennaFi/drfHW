723e91b42508848a90f9decb9452cc02c866839a
JennaFi <fi@mail.com>
2025-03-14 02:59:24 +0100

    hw 35_2

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret')
 
 DEBUG = True if os.getenv('DEBUG') == 'True' else False
 
